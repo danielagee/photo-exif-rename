e6386e3e7f050036e9384eae2fa9908a2b5341a5
danielagee <danielhouston15@yahoo.com>
2022-11-02 17:26:01 +0200

    Specifically print the date taken.

--- run.py
+++ run.py
@@ -1,21 +1,13 @@
-# Day 41 - Install and test exifread package.
+# Day 42 - Extract just the date tag
 
-# The exifread package seems to have quite a bit of functionality. Recommend to read through the details!
-# https://github.com/ianare/exif-py/blob/develop/README.rst
-# https://pypi.org/project/ExifRead/
-
-# An extensive list of exif tags can be found here: https://exiftool.org/TagNames/EXIF.html
-
-# Import the ExifRead package. This has the functionality we need to extract the metadata from the photos.
-# Painful lesson learned here... PyCharm does not automatically import PIP installed packages. You have to
-# manually import them to the project using File > Settings > Project > Project Interpreter, Press the + button
-# Type "exifread" in the top search box, Press Install Package
-
+# Import the ExifRead package.
 import exifread
 
-# Open image file for reading (must be in binary mode)
-image_file = open('C:\\Python\\image1.jpg', 'rb')
+# A "with" statement allows us to cycle through the tags until we reach the one we want
+# https://www.geeksforgeeks.org/with-statement-in-python/
+# https://docs.python.org/3/reference/compound_stmts.html#the-with-statement
+with open('C:\\Python\\image1.jpg', 'rb') as image_file:
+    tags = exifread.process_file(image_file, stop_tag="EXIF DateTimeOriginal")
+    dateTaken = tags["EXIF DateTimeOriginal"]
 
-# Return Exif tags
-tags = exifread.process_file(image_file)
-print(tags)
+print(dateTaken)
