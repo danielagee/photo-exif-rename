0d2d4dcea4cf37ab27c533d59c23f90c5ae134af
danielagee <danielhouston15@yahoo.com>
2022-11-06 08:54:13 +0200

    Use the os.rename method to rename the file with the new image file name.

--- run.py
+++ run.py
@@ -1,30 +1,28 @@
-# Day 45 - Concatenate and append date into original file name
+# Day 45 - Rename the file
 
 # Import the ExifRead package.
 import exifread
 
-# First, we need to pull out the path and image file name as separate variables
-# to use the file name in the end result.
+# Import the OS package to use the os.rename module.
+import os
+
 path = 'C:\\Python\\'
 image_file = 'image1.jpg'
 
 # "With" statement to cycle through the tags until we reach the one we want.
-# Replacing the hard coded file name with the variables gives the below.
 with open(path+image_file, 'rb') as image_tags:
     tags = exifread.process_file(image_tags, stop_tag="EXIF DateTimeOriginal")
     dateTaken = tags["EXIF DateTimeOriginal"]
 
 year = str(dateTaken)[0:4]
-print(year)
 month = str(dateTaken)[5:7]
-print(month)
 day = str(dateTaken)[8:10]
-print(day)
 
-# There are multiple ways to achieve the concatenation but f-string seems to be
-# the preferred, and most elegant, way to do so.
-# https://peps.python.org/pep-0498/
+# Concatenate the year, month, day, and existing file name to make the new file name.
 new_name = f'{year}-{month}-{day} - {image_file}'
-# new_name = "%s-%s-%s - %s" % (year, month, day, image_file)
-# new_name = year+'-'+month+'-'+day+' - '+str(image_file)
 print(new_name)
+
+# Use os.rename method to rename the file.
+# https://docs.python.org/3/library/os.html#:~:text=os.rename(,src%20and%20dst.
+os.rename(path+image_file, path+new_name)
+print("File renamed.")
