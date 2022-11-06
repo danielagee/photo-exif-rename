712ef7fccc893ecf2eacd46ed1aa8480864581a2
danielagee <danielhouston15@yahoo.com>
2022-11-06 09:35:42 +0200

    Integrate the full code from day 45 to complete the task of renaming files in a folder.

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-# Day 46 - Extract list of all files in the root folder.
-# This will be done by integrating the Day 5 code with code from the
+# Day 46 - Extract list of all files in the root folder and loop to rename.
+# This will be done by integrating the Day 45 code with code from the
 # Backup Tool project.
 
 import exifread
 import os
-
-# Import the glob function to get a list of image files.
 import glob
 
 root_path = 'C:\\Python\\pictures\\'
+root_path_original = root_path
 
 # Adds '**' to the user specified root directory so that Python will only
 # search the root and subdirectories.
@@ -19,23 +18,18 @@ file_list = glob.glob(root_path, recursive=True)
 
 # For loop to write a parsed list of all missing backup files to the text document.
 for item in file_list:
-    print(str(item))
+    # "With" statement to cycle through the tags until we reach the one we want.
+    with open(item, 'rb') as image_tags:
+        tags = exifread.process_file(image_tags, stop_tag="EXIF DateTimeOriginal")
+        dateTaken = tags["EXIF DateTimeOriginal"]
 
-""" #Temporarily commenting out unused code to test the extract code
-# image_file = 'image1.jpg'
+    year = str(dateTaken)[0:4]
+    month = str(dateTaken)[5:7]
+    day = str(dateTaken)[8:10]
 
-# "With" statement to cycle through the tags until we reach the one we want.
-with open(path+image_file, 'rb') as image_tags:
-    tags = exifread.process_file(image_tags, stop_tag="EXIF DateTimeOriginal")
-    dateTaken = tags["EXIF DateTimeOriginal"]
-
-year = str(dateTaken)[0:4]
-month = str(dateTaken)[5:7]
-day = str(dateTaken)[8:10]
-
-# Concatenate the year, month, day, and existing file name to make the new file name.
-new_name = f'{year}-{month}-{day} - {image_file}'
-print(new_name)
-os.rename(path+image_file, path+new_name)
-print("File renamed.")
-"""
+    # variable 'item' contains the full path name. Short code to trim that out...
+    original_file_name = item.replace(root_path_original, '')
+    # Concatenate the year, month, day, and existing file name to make the new file name.
+    new_name = f'{year}-{month}-{day} - {original_file_name}'
+    os.rename(item, root_path_original+new_name)
+print("Files renamed.")
