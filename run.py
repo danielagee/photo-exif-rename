863a7e1facb9a4d2b2503694061e5511e18dd7bc
danielagee <danielhouston15@yahoo.com>
2022-11-06 10:08:00 +0200

    Testing on a larger, mixed data set caused the program to fail due to missing exif tags. Added a few lines to prevent this by skipping files that are not .jpg or .JPG and modifying the code to return "none" if the tag was missing rather than failing.

--- run.py
+++ run.py
@@ -1,35 +1,38 @@
-# Day 46 - Extract list of all files in the root folder and loop to rename.
-# This will be done by integrating the Day 45 code with code from the
-# Backup Tool project.
+# Day 47 - Testing on a bigger data set
+# Running on the full data set immediately raises failures. Missing tags, wrong file types, etc.
+# Fixes used:
+# 1) Swap dateTaken = tags["EXIF DateTimeOriginal"] with dateTaken = tags.get("EXIF DateTimeOriginal")
+#       a) The original code fails if the tag is missing. The new code returns None which I can then
+#           skip with an "if date_taken != None:" statement
+# 2) added "if item.endswith(('.jpg', '.JPG')):" to skip non-jpg files.
+# 3) added "if dateTaken is not None:" to skip files without exif date tags
 
 import exifread
 import os
 import glob
 
 root_path = 'C:\\Python\\pictures\\'
-root_path_original = root_path
 
-# Adds '**' to the user specified root directory so that Python will only
-# search the root and subdirectories.
-root_path += '*.*'
-
-# Extracts the active and backup file lists from the user specified folders.
-file_list = glob.glob(root_path, recursive=True)
+# Extracts the file list.
+# The root path is needed later, but *.* is only needed once.
+# Consolidated here and removed the root_path_original variable.
+file_list = glob.glob(root_path+'*.*', recursive=True)
+print("Reading and renaming files.")
 
 # For loop to write a parsed list of all missing backup files to the text document.
 for item in file_list:
-    # "With" statement to cycle through the tags until we reach the one we want.
-    with open(item, 'rb') as image_tags:
-        tags = exifread.process_file(image_tags, stop_tag="EXIF DateTimeOriginal")
-        dateTaken = tags["EXIF DateTimeOriginal"]
+    # if statement filters for only jpg or JPG files
+    if item.endswith(('.jpg', '.JPG')):
+        # with statement allows us to cycle through the tags
+        with open(item, 'rb') as image_tags:
+            tags = exifread.process_file(image_tags, stop_tag="EXIF DateTimeOriginal")
+            dateTaken = tags.get("EXIF DateTimeOriginal")
 
-    year = str(dateTaken)[0:4]
-    month = str(dateTaken)[5:7]
-    day = str(dateTaken)[8:10]
-
-    # variable 'item' contains the full path name. Short code to trim that out...
-    original_file_name = item.replace(root_path_original, '')
-    # Concatenate the year, month, day, and existing file name to make the new file name.
-    new_name = f'{year}-{month}-{day} - {original_file_name}'
-    os.rename(item, root_path_original+new_name)
+        # if statement filters out files with no date tags
+        if dateTaken is not None:
+            year = str(dateTaken)[0:4]
+            month = str(dateTaken)[5:7]
+            day = str(dateTaken)[8:10]
+            new_name = f'{year}-{month}-{day} - {item.replace(root_path, "")}'
+            os.rename(item, root_path+new_name)
 print("Files renamed.")
