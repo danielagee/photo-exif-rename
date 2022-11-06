a95c887aaf2ef961113e429413fd7b7e57052ced
danielagee <danielhouston15@yahoo.com>
2022-11-06 09:11:13 +0200

    Integrated code from the backup tool project to extract a list of all files in the root folder. Commented out existing code to test that the extract code works properly.

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
-# Day 45 - Rename the file
+# Day 46 - Extract list of all files in the root folder.
+# This will be done by integrating the Day 5 code with code from the
+# Backup Tool project.
 
-# Import the ExifRead package.
 import exifread
-
-# Import the OS package to use the os.rename module.
 import os
 
-path = 'C:\\Python\\'
-image_file = 'image1.jpg'
+# Import the glob function to get a list of image files.
+import glob
+
+root_path = 'C:\\Python\\pictures\\'
+
+# Adds '**' to the user specified root directory so that Python will only
+# search the root and subdirectories.
+root_path += '*.*'
+
+# Extracts the active and backup file lists from the user specified folders.
+file_list = glob.glob(root_path, recursive=True)
+
+# For loop to write a parsed list of all missing backup files to the text document.
+for item in file_list:
+    print(str(item))
+
+""" #Temporarily commenting out unused code to test the extract code
+# image_file = 'image1.jpg'
 
 # "With" statement to cycle through the tags until we reach the one we want.
 with open(path+image_file, 'rb') as image_tags:
@@ -21,8 +36,6 @@ day = str(dateTaken)[8:10]
 # Concatenate the year, month, day, and existing file name to make the new file name.
 new_name = f'{year}-{month}-{day} - {image_file}'
 print(new_name)
-
-# Use os.rename method to rename the file.
-# https://docs.python.org/3/library/os.html#:~:text=os.rename(,src%20and%20dst.
 os.rename(path+image_file, path+new_name)
 print("File renamed.")
+"""
